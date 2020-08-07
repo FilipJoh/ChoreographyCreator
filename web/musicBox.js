82bf29e14b481e3101bb58c8491d01f17fbff10c
Filip Johannesson <filip.johannesson@gmail.com>
2020-08-07 14:26:29 +0200

    Use wavesurfer for experiments

--- web/musicBox.js
+++ web/musicBox.js
@@ -6,15 +6,19 @@ elms.forEach(function(elm) {
   window[elm] = document.getElementById(elm);
 });
 
-
-
-
 // function constructor for player, the class containing the howler instance and playback functions
 var player = function(){
   this.test = 0;
   this.sound = null;
   this.playId = null;
   this.pauseTime = null;
+  this.visual = WaveSurfer.create({
+      container: '#waveform',
+      scrollParent: true,
+      plugins: [
+       WaveSurfer.regions.create({})
+     ]
+  });
 };
 player.prototype = {
   playMusic: function() {
@@ -22,18 +26,22 @@ player.prototype = {
         src: ["../data/Rasputin.mp3"],
         html5: true
     });
-    this.playId = this.sound.play();
+
+    //this.visual.load("../data/Rasputin.mp3")
+    //this.playId = this.sound.play();
     if (this.pauseTime != null) {
       this.sound.seek(this.pauseTime, this.playId);
     }
-
+    this.visual.play();
+    this.visual.addRegion(id = "test", start = 0.0, end = 10.0)
     playBtn.style.display = 'none';
     pauseBtn.style.display = 'block';
   },
 
   pauseMusic: function() {
     if (this.sound != null) {
-      this.sound.pause();
+      //this.sound.pause();
+      this.visual.pause();
       this.pauseTime = this.sound.seek(this.playId);
       pauseBtn.style.display = 'none';
       playBtn.style.display = 'block';
@@ -52,15 +60,24 @@ player.prototype = {
     } else {
       console.log("sound is null");
     }
+    this.visual.stop();
+    console.log("Current regions:");
+    for (var index = 0; this.visual.regions.list.length; index++ )
+    {
+      console.log(this.visual.regions.list[index].id.toString());
+    }
   }
 };
 
-// actual ovject creation
+// actual object creation
 var player = new player();
 
 // Bind our player controls.
 playBtn.addEventListener('click', function() {
   player.playMusic();
+  //wavesurfer.load('../data/Rasputin.mp3');
+
+  //wavesurfer.play();
 });
 pauseBtn.addEventListener('click', function() {
   player.pauseMusic();
@@ -71,3 +88,27 @@ stopBtn.addEventListener('click', function() {
 /*nextBtn.addEventListener('click', function() {
   player.skip('next');
 });*/
+
+// Once the user loads a file in the fileinput, the file should be loaded into waveform
+    document.getElementById("fileinput").addEventListener('change', function(e){
+        var file = this.files[0];
+
+        if (file) {
+            var reader = new FileReader();
+
+            reader.onload = function (evt) {
+                // Create a Blob providing as first argument a typed array with the file buffer
+                var blob = new window.Blob([new Uint8Array(evt.target.result)]);
+
+                // Load the blob into Wavesurfer
+                player.visual.loadBlob(blob);
+            };
+
+            reader.onerror = function (evt) {
+                console.error("An error ocurred reading the file: ", evt);
+            };
+
+            // Read File as an ArrayBuffer
+            reader.readAsArrayBuffer(file);
+        }
+    }, false);
