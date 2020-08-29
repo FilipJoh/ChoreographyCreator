25c0f695a7dd7e2e0dd4d8c1d0641945b4cd908f
Filip Johannesson <filip.johannesson@gmail.com>
2020-08-29 20:12:02 +0200

    more region handling

--- web/musicBox.js
+++ web/musicBox.js
@@ -9,6 +9,7 @@ elms.forEach(function(elm) {
 // function constructor for player, the class containing the howler instance and playback functions
 var player = function(){
   this.test = 0;
+  this.regCounter = 0;
   this.sound = null;
   this.playId = null;
   this.pauseTime = null;
@@ -20,8 +21,23 @@ var player = function(){
        WaveSurfer.regions.create()
      ]
   });
-  this.visual.addRegion({id: "test", start: 0, end: 30, color: "rgba(255.0, 0.0, 0.0, 0.2)"});
-  this.visual.enableDragSelection({id: "test_2", color: "rgba(255.0, 0.0, 0.0, 0.8)"})
+
+  that = this
+  // Event capture for region stuff, using anonumous function now
+  this.visual.on('region-created', function(region) {
+    console.log("region created!!");
+
+    that.regCounter++;
+    region.id = region.id.concat((that.regCounter).toString());
+  })
+
+  this.visual.on('region-mouseenter', function(region) {
+    console.log("entered region: %s", region.id)
+  })
+
+  this.visual.addRegion({id: "test_", start: 0, end: 30, color: "rgba(255.0, 0.0, 0.0, 0.5)"});
+  this.visual.enableDragSelection({id: "test_2_", color: "rgba(255.0, 0.0, 0.0, 0.8)"});
+
 };
 player.prototype = {
   playMusic: function() {
@@ -50,9 +66,6 @@ var player = new player();
 // Bind our player controls.
 playBtn.addEventListener('click', function() {
   player.playMusic();
-  //wavesurfer.load('../data/Rasputin.mp3');
-
-  //wavesurfer.play();
 });
 pauseBtn.addEventListener('click', function() {
   player.pauseMusic();
